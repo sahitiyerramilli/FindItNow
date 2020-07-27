034a7134629cbeca81d5969a25db7b7953e788fe
Jayant Sravan <jatamara@microsoft.com>
2020-07-27 17:51:28 +0530

    Added more code

--- Background.js
+++ Background.js
@@ -1,8 +1,21 @@
 var seltext = null;
     
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse)
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
 {
     seltext = request.greeting;
-    Console.log("done");
+
+    fetch('https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=es', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Ocp-Apim-Subscription-Key': '3cb97d073a764608af07f2857d578cee',
+            'Ocp-Apim-Subscription-Region': 'centralindia'
+        },
+        body: JSON.stringify([
+            { 'Text': 'Hey, this is awesome' }
+        ])
+    }).then(res => {
+        return res.json()
+    }).then(data => console.log(data))
 }
 );
