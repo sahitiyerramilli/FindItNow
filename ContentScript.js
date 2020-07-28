cc6a7ea599e1424893e9f45910c36f69beb2ea3c
Sahiti Y <sahy@microsoft.com>
2020-07-28 23:08:32 +0530

    overlay code

--- ContentScript.js
+++ ContentScript.js
@@ -1,6 +1,25 @@
 document.addEventListener('mouseup', function (event) {
     var sel = window.getSelection().toString();
-    console.log("hello"+sel);
+    console.log("hello" + sel);
+    var popup = document.createElement("DIV");
+    popup.text = "selected";
+    popup.id = "tooltip";
+    document.body.appendChild(popup);
+    $("#tooltip").css({
+         top: event.pageY + 'px',
+        left: event.pageX + 'px',
+         position : 'absolute'
+    });
+    $('#tooltip').css('background-color', '#EEE');
+    $('#tooltip').css('display', 'inline-block');
+    $('#tooltip').css('padding-top', '5px');
+    $('#tooltip').css('padding-right', '5px');
+    $('#tooltip').css('padding-bottom', '5px');
+    $('#tooltip').css('padding-left', '5px');
+    $('#tooltip').css('border', '1px solid');
+    $('#tooltip').css('border-color', '#3333FF');
+    $('#tooltip').css('border-radius', '2px');
+    $('#tooltip').css('display', 'none'); 
     if (sel.length)
         chrome.runtime.sendMessage({ greeting : sel }, function (response) { })
 })
@@ -10,8 +29,8 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
     console.log("onchanged");
     chrome.storage.sync.get(['response'], function (result) {
         console.log("hi " + result.key);
-        /*var popup = document.getElementById("myPopup");
-        popup.innerHTML = "hello";
-        popup.classList.toggle("show");*/
+        console.log("Tootltip " + $("#tooltip"));
+        $("#tooltip").text("this is a para");
+        $("#tooltip").show();
     });
 })
