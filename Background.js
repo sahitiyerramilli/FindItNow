50c3c4f74c40f2423d41c9368167172478918bad
Sahiti Y <sahy@microsoft.com>
2020-07-29 20:46:47 +0530

    ui changes

--- Background.js
+++ Background.js
@@ -4,31 +4,49 @@ var languageCode = 'hi';
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
 {
     seltext = request.greeting;
-    console.log("background");
-    if (seltext.split(' ').length < 2) {
-        fetch('https://api.dictionaryapi.dev/api/v1/entries/en/' + seltext).then(res => {
-            return res.json()
-        }).then(data =>
-            //console.log("Object" + (data[0].meaning
-            chrome.storage.sync.set({ meaning: (data[0].meaning[Object.keys(data[0].meaning)[0]])[0].definition }, function () { }));
+    seltext = seltext.trim();
+    var localDictionary = {
+        'SMTP': 'Simple Mail Transfer Protocol',
+        'SSA': 'Search Service Application (SPO/ GL)',
+        'SSDB': 'Search Setting DB (SPO/ GL)',
+        'SSMS': 'Substrate Search management Service (SPO/ GL)',
+        'SOA': 'Service Oriented Architecture',
+        'SIGS': 'Substrate Signal Service'
+    }
+    console.log("background " + seltext.split(' ') + " " + seltext.split(' ').length);
+    if (seltext in localDictionary) {
+        chrome.storage.sync.set({ meaning: localDictionary[seltext] }, function () { });
+        chrome.storage.sync.set({ response: "" }, function () { });
     }
     else {
-        chrome.storage.sync.set({ meaning: "" }, function () { });
-    }
 
-    fetch('https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=' + languageCode, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Ocp-Apim-Subscription-Key': '3cb97d073a764608af07f2857d578cee',
-            'Ocp-Apim-Subscription-Region': 'centralindia'
-        },
-        body: JSON.stringify([
-            { 'Text': seltext }
-        ])
-    }).then(res => {
-        return res.json()
-    }).then(data => chrome.storage.sync.set({ response: data[0].translations[0].text }, function () { }));
+        if (seltext.split(' ') == null || seltext.split(' ').length < 2) {
+            console.log("entered");
+            fetch('https://api.dictionaryapi.dev/api/v1/entries/en/' + seltext).then(res => {
+                return res.json()
+            }).then(data =>
+                //console.log("Object" + (data[0].meaning
+                chrome.storage.sync.set({ meaning: (data[0].meaning[Object.keys(data[0].meaning)[0]])[0].definition }, function () { }));
+        }
+        else {
+            chrome.storage.sync.set({ meaning: "" }, function () { });
+        }
+
+        fetch('https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=' + languageCode, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Ocp-Apim-Subscription-Key': '3cb97d073a764608af07f2857d578cee',
+                'Ocp-Apim-Subscription-Region': 'centralindia'
+            },
+            body: JSON.stringify([
+                { 'Text': seltext }
+            ])
+        }).then(res => {
+            return res.json()
+        }).then(data => chrome.storage.sync.set({ response: data[0].translations[0].text }, function () { }));
+
+    }
 })
 
 chrome.storage.onChanged.addListener(function (changes, namespace) {
--- ContentScript.js
+++ ContentScript.js
@@ -11,23 +11,25 @@ document.addEventListener('mouseup', function (event) {
     popup.id = "tooltip";
     document.body.appendChild(popup);
     $("#tooltip").css({
-         top: event.pageY + 'px',
+        top: event.pageY + 'px',
         left: event.pageX + 'px',
-         position : 'absolute'
+        position: 'absolute'
     });
-    $('#tooltip').css('background-color', '#EEE');
+    $('#tooltip').css('background-color', '#ADD8E6');
     $('#tooltip').css('display', 'inline-block');
     $('#tooltip').css('padding-top', '5px');
     $('#tooltip').css('padding-right', '5px');
     $('#tooltip').css('padding-bottom', '5px');
     $('#tooltip').css('padding-left', '5px');
-    $('#tooltip').css('border', '1px solid');
-    $('#tooltip').css('border-color', '#3333FF');
+    $('#tooltip').css('fontSize', '17px');
+    //$('#tooltip').css('border', '1px solid');
+    //$('#tooltip').css('border-color', '#3333FF');
+    $('#tooltip').css('font-style', 'Microsoft GothicNeo');
     $('#tooltip').css('border-radius', '2px');
-    $('#tooltip').css('display', 'none'); 
+    $('#tooltip').css('display', 'none');
     console.log(sel)
-    if (sel.length && sel.split(' ').length<50)
-        chrome.runtime.sendMessage({ greeting : sel }, function (response) { })
+    if (sel.length && sel.split(' ').length < 50)
+        chrome.runtime.sendMessage({ greeting: sel }, function (response) { })
 })
 
 chrome.storage.onChanged.addListener(function (changes, namespace) {
@@ -53,8 +55,7 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
     }
 })
 
-function displayData()
-{
+function displayData() {
     var sel = window.getSelection().toString();
     if (count == 2) {
         var data = sel;
@@ -62,8 +63,10 @@ function displayData()
             data += "<br><br><b>Meaning:</b> " + meaning
         else
             data += "<br>";
-        data += "<br><b>Translation:</b> " + translation;
+        if (translation != "")
+            data += "<br><b>Translation:</b> " + translation;
         document.getElementById("tooltip").innerHTML = data;
+        $("#tooltip").fadeTo("fast",0.85);
         $("#tooltip").show();
         count = 0;
     }
